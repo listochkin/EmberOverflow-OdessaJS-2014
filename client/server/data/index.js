f55e5ed01343e8e4e503bc21f891a1e41f28781d
Andrey Listochkin <listochkin@outlook.com>
2014-07-05 12:28:11 +0000

    Questions and Answers in Russian

--- client/server/data/index.js
+++ client/server/data/index.js
@@ -1,18 +1,18 @@
 var users = [
-  { id: 11, name: 'Artyom Trityak', twitter: 'artyomtrityak', password: '123', questions: [21] },
-  { id: 12, name: 'Eldar Djafarov', twitter: 'edjafarov', password: '145', answers: [31] },
-  { id: 13, name: 'Ingvar Stepanyan', twitter: 'RReverser', password: '178', answers: [32] },
-  { id: 14, name: 'Maxim Klymyshyn', twitter: 'maxmaxmaxmax', password: '167', answers: [33] }
+  { id: 11, name: 'Артем Тритьяк', twitter: 'artyomtrityak', password: '123', questions: [21] },
+  { id: 12, name: 'Эльдар Джафаров', twitter: 'edjafarov', password: '145', answers: [31] },
+  { id: 13, name: 'Ингвар Степанян', twitter: 'RReverser', password: '178', answers: [32] },
+  { id: 14, name: 'Максим Климишин', twitter: 'maxmaxmaxmax', password: '167', answers: [33] }
 ];
 
 var questions = [
-  { id: 21, title: 'How ca I add two values in JavaScript?', body: 'I have numbers 2 and 3 and I have no idea how I can add them together in JavaScript. Can you guys help me?', user: 11, answers: [31, 32, 33] }
+  { id: 21, title: 'Как в JS можно числа складывать?', body: 'Чеваки, я чегго-то не могу понять, как мне в JavaScript сложить 2 и 3. Я уже три часа ковыряюсь и толку никакого.', user: 11, answers: [31, 32, 33] }
 ];
 
 var answers = [
-  { id: 31, body: "I don't know, dude. May be there's a jQuery plugin for that?", user: 12, question: 21 },
-  { id: 32, body: "V8's JIT is so fast, no need to add numbers - it will optimize all the results away!", user: 13, question: 21 },
-  { id: 33, body: "What about just adding them with +???", user: 14, question: 21 }
+  { id: 31, body: "Чувак, я тебе советую  попробовать Реакт - он офигенен! Я не пробовал сам числа складывать, но уверен, что Реакт это умеет.", user: 12, question: 21 },
+  { id: 32, body: "Так JIT в V8 все равно все заинлайнит, и числа складывать совсем не придется.", user: 13, question: 21 },
+  { id: 33, body: "Чувак, ты больной??? 2 + 3", user: 14, question: 21 }
 ];
 
 var next = 100;
